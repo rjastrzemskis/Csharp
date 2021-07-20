250ddc5fc7995e9a10a8bed8f85e20fe558be57d
rjastrzemskis <83876094+rjastrzemskis@users.noreply.github.com>
2021-07-20 20:52:52 +0300

    Update Program.cs

--- csharp-basics/exercises/Loops/Loops/Piglet/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Piglet/Program.cs
@@ -14,25 +14,25 @@ namespace Piglet
             bool playing = true;
             while (playing)
             {
-                    Random random = new Random();
-                    int randomNumber = random.Next(1, 6);
-                    if (randomNumber == 1)
-                    {
-                        Console.WriteLine("You rolled a 1! You got 0 points. The game is over");
-                        playing = false;
-                    }
-                    else if (answer == "no")
-                    {
-                        Console.WriteLine("Your score is " + score + ". The game is over.");
-                        playing = false;
-                    }
-                    else
-                    { 
+                Random random = new Random();
+                int randomNumber = random.Next(1, 6);
+                if (randomNumber == 1)
+                {
+                    Console.WriteLine("You rolled a 1! You got 0 points. The game is over");
+                    playing = false;
+                }
+                else if (answer == "no")
+                {
+                    Console.WriteLine("Your score is " + score + ". The game is over.");
+                    playing = false;
+                }
+                else
+                {
                     score += randomNumber;
                     Console.WriteLine("You rolled a " + randomNumber + "!");
                     Console.Write("Roll again? ");
                     answer = Console.ReadLine();
-                    }
+                }
             }
             Console.ReadKey();
         }
