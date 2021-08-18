abca2e1b72a83e79a2bcb3b51c499c96fd7b6684
rjastrzemskis <83876094+rjastrzemskis@users.noreply.github.com>
2021-08-18 14:12:43 +0300

    Initial commit

---
+++ csharp-basics/exercises/Tests/Arithmetic/CalculateArea.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculateArea.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculateArea.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e7be56f0-edc6-4594-8aa4-eb3e7bd53cae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
