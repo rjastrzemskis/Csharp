db452422b37221706988d9c8890c10aa50c0b193
rjastrzemskis <83876094+rjastrzemskis@users.noreply.github.com>
2021-08-20 00:49:54 +0300

    Update Geometry.cs

--- csharp-basics/exercises/Arithmetic/CalculateArea/Geometry.cs
+++ csharp-basics/exercises/Arithmetic/CalculateArea/Geometry.cs
@@ -6,16 +6,22 @@ namespace CalculateArea
     {
         public static double areaOfCircle(decimal radius)
         {
+            if (radius < 0)
+                throw new ArgumentException("You entered negative number!");
             return Math.PI * Math.Pow((double)radius, 2);
         }
 
         public static double areaOfRectangle(decimal length, decimal width)
         {
+            if (length < 0 || width < 0)
+                throw new ArgumentException("You entered negative number!");
             return (double)length * (double)width;
         }
 
         public static double areaOfTriangle(decimal ground, decimal h)
         {
+            if (ground < 0 || h < 0)
+                throw new ArgumentException("You entered negative number!");
             return (double)ground * (double)h * 0.5;
         }
     }
