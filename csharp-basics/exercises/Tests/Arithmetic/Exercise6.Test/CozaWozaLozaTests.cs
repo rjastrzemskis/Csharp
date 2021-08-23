a53ffefa9b25e87576a0d8423a7fcc9641b4dbd4
rjastrzemskis <83876094+rjastrzemskis@users.noreply.github.com>
2021-08-23 15:26:56 +0300

    Update CozaWozaLozaTests.cs

--- csharp-basics/exercises/Tests/Arithmetic/Exercise6.Test/CozaWozaLozaTests.cs
+++ csharp-basics/exercises/Tests/Arithmetic/Exercise6.Test/CozaWozaLozaTests.cs
@@ -11,14 +11,14 @@ namespace Exercise6.Test
             //Arrange
             int lowerBound = 1;
             int upperBound = 33;
-
+            string expected = "1 2 Coza 4 Loza Coza Woza 8 Coza Loza 11 \n" +
+                              "Coza 13 Woza CozaLoza 16 17 Coza 19 Loza CozaWoza 22 \n" +
+                              "23 Coza Loza 26 Coza Woza 29 CozaLoza 31 32 Coza";
             //Act
             string result = CozaWozaLoza.CozaLozaWozaSwap(lowerBound, upperBound);
 
             //Assert
-            Assert.AreEqual("1 2 Coza 4 Loza Coza Woza 8 Coza Loza 11 \n" +
-                            "Coza 13 Woza CozaLoza 16 17 Coza 19 Loza CozaWoza 22 \n" +
-                            "23 Coza Loza 26 Coza Woza 29 CozaLoza 31 32 Coza", result);
+            Assert.AreEqual(expected, result);
         }
     }
 }
